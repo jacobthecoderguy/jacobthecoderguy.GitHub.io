257423c271b4b186f8252c55a286ac7f85e94579
jacobthecoderguy <116555009+jacobthecoderguy@users.noreply.github.com>
2023-03-24 10:32:46 +0000

    db loading test

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 async function loadDB() {
     let dbObject = await fetch("datebase.json");
-    let txtdat = await myObject.text();
+    let txtdat = await dbObject.text();
     let jsonval = await JSON.parse(txtdat);
     return jsonval;
 }
 
-console.log(loadDB);
+alert(loadDB);
