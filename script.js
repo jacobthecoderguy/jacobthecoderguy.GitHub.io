ea34d2ee21d9922a7d05559038dc93974a8629b8
jacobthecoderguy <116555009+jacobthecoderguy@users.noreply.github.com>
2023-03-24 10:20:16 +0000

    added datebase loading to script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-var dbpath = "database.json"
+async function loadDB() {
+    let dbObject = await fetch("datebase.json");
+    let txtdat = await myObject.text();
+    let jsonval = await JSON.parse(txtdat);
+    return jsonval;
+}
